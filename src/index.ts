74e2c6a42b1098068943fdb7ba57c71359b883aa
jdw5 <joshuadwallace1@gmail.com>
2024-11-25 18:22:03 +1000

    Resolved missing types declaration file when bundling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-/** Your exports */
-
 export { useQuery } from "./use-query";
 export type { UseQueryProps, QueryValue } from "./types";
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,32 @@
 import { resolve } from "path"
 import { defineConfig } from "vite"
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [dts({
+      exclude: ["src/test/**"],
+      rollupTypes: true,
+      insertTypesEntry: true,
+      include: ["src/**/*.ts", "src/**/*.d.ts", "src/**/*.tsx", "src/**/*.vue"],
+      outDir: "dist",
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "Query",
+      name: 'query',
       fileName: 'index',
     },
     rollupOptions: {
-      external: [],
+      external: ['vue', '@vueuse/core', '@inertiajs/vue3', 'axios'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          '@vueuse/core': 'VueUse',
+          '@inertiajs/vue3': 'InertiaVue',
+          axios: 'axios',
+        },
+      },
     },
   },
 })
